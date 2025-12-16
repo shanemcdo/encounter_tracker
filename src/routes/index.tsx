8a10f2b2ad9ab5267a19ee68c3dde2d38049965a
Shane McDonough <shanemcdman@gmail.com>
2025-12-16 07:40:32 -0800

    use fs module to read json to get data for encounter

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,6 @@ import Creature from "~/components/Creature";
 
 function getEncounter(): Encounter {
 	"use server";
-	return [
-		{
-			name: "Ghost Pirate",
-			max_hp: 50
-		},
-		{
-			name: "Vampire Spawn",
-			max_hp: 70
-		}
-	];
 	const file = readFileSync('example.json', 'utf-8');
 	return JSON.parse(file);
 }
