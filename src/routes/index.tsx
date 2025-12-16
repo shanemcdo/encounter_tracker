8ad8471950b5ef4572bd4e6089e4e23401c392e3
Shane McDonough <shanemcdman@gmail.com>
2025-12-16 07:47:45 -0800

    use fs/promises instead

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,19 @@
+import { createAsync } from "@solidjs/router";
 import { For } from "solid-js";
-import { readFileSync } from "fs";
+import { readFile } from "fs/promises";
 import Creature from "~/components/Creature";
 
-function getEncounter(): Encounter {
+async function getEncounter(): Promise<Encounter> {
 	"use server";
-	const file = readFileSync('example.json', 'utf-8');
+	const file = await readFile('example.json', 'utf-8');
 	return JSON.parse(file);
 }
 
 export default function Home() {
-	const encounter = getEncounter();
+	const encounter = createAsync(getEncounter);
 	return (
 		<main>
-			<For each={encounter}>{ creature =>
+			<For each={encounter()}>{ creature =>
 				<Creature creature={creature} />
 			}</For>
 		</main>
