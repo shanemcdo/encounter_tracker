a6a7afdefba5d6dbf94e901d6c611194567c6f4e
Shane McDonough <shanemcdman@gmail.com>
2025-12-16 08:58:03 -0800

    get some basic directory chosing working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,38 @@
+import { createAsync } from "@solidjs/router";
+import { glob } from "fs/promises";
+import { createSignal, For, untrack } from "solid-js";
+
+async function getFiles(cwd: string) {
+	"use server";
+	const result = [];
+	for await(const file of glob('*.json', { cwd })) {
+		result.push(file);
+	}
+	return result;
+}
+
+function getCwd() {
+	"use server";
+	return process.cwd();
+}
+
 export default function Home() {
+	const [cwd, setCwd] = createSignal(getCwd());
+	const files = createAsync(() => getFiles(cwd()));
 	return (
 		<main>
-			<form
-				action="/encounter"
-				method="get"
-			>
-				<label for="encounter">Encounter:</label>
-				<input
-					type="file"
-					id="encounter"
-					name="encounter"
-					accept=".json"
-				/>
-				<button type="submit">Submit</button>
-			</form>
+			<label for="cwd">Directory:</label>
+			<input
+				id="cwd"
+				value={untrack(cwd)}
+				onchange={event => {
+					setCwd(event.currentTarget.value);
+				}}
+			/>
+			<br />
+			<For each={files()}>{ file =>
+				<a href={`encounter/?encounter=${cwd()}/${file}`}>{file}</a>
+			}</For>
 		</main>
-	); }
+	);
+}
